02253520e8d149a75d5858136dde21748a6e303b
Alex <truonggiang190689@gmail.com>
2025-01-21 03:16:23 +0700

    add layout components, theme context, and configuration files

---
+++ components/theme/theme-context.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+    createContext,
+    ReactNode,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextProps {
+    theme: Theme;
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    const [theme, setTheme] = useState<Theme>(() => {
+        if (typeof window !== "undefined") {
+            const storedTheme = localStorage.getItem("theme") as Theme;
+            return storedTheme || "light";
+        }
+        return "light";
+    });
+
+    useEffect(() => {
+        if (typeof window !== "undefined") localStorage.setItem("theme", theme);
+
+        document.documentElement.classList.remove("light", "dark");
+        document.documentElement.classList.add(theme);
+    }, [theme]);
+
+    const toggleTheme = () =>
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context) throw new Error("useTheme must be used within a ThemeProvider");
+
+    return context;
+};
---
+++ components/theme/theme-switcher.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "./theme-context";
+
+const ThemeSwitcher = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="flex w-full items-center gap-2.5 p-2.5"
+    >
+      <span>
+        {theme === "light" ? (
+          <Moon strokeWidth={1} size={22} />
+        ) : (
+          <Sun strokeWidth={1} size={22} />
+        )}
+      </span>
+      <p> Go {theme === "light" ? "dark" : "light"}</p>
+    </button>
+  );
+};
+
+export default ThemeSwitcher;
